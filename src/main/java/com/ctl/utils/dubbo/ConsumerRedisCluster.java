15872884a73b08cd5af83704d3d166589d02525a
林家小院 <guolinit@163.com>
2018-05-10 18:15:27 +0800

    update

--- src/main/java/com/ctl/utils/dubbo/ConsumerRedisCluster.java
+++ src/main/java/com/ctl/utils/dubbo/ConsumerRedisCluster.java
@@ -4,7 +4,9 @@ import com.ctl.web.dubbo.service.HelloService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 运行时需要启动zooKeeper ,需要执行Provider或者启动tomcat服务，启动tomcat前需要已经启动zookeeper
+ * 运行时需要启动redis ,需要执行Provider或者启动tomcat服务，启动tomcat前需要已经启动redis
+ * 如果服务的在但是redis停止服务,依旧可以访问接口
+ * redis://192.168.42.29:6379?backup=192.168.42.29:6380,192.168.42.29:6381 如果6381关闭这时6383会变成master 这是如果 配置文件不变就会报错
  */
 public class ConsumerRedisCluster {
     public static void main(String[] args) throws Exception {
