1890067c3ad611382081b163c3ad51cc9aa72108
林家小院 <guolinit@163.com>
2018-05-29 17:32:31 +0800

    zookeeper update

--- src/main/java/com/ctl/utils/dubbo/ConsumerZookeeper.java
+++ src/main/java/com/ctl/utils/dubbo/ConsumerZookeeper.java
@@ -4,7 +4,7 @@ import com.ctl.web.dubbo.service.HelloService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 运行时需要启动zooKeeper ,需要执行Provider或者启动tomcat服务，启动tomcat前需要已经启动zookeeper
+ * 运行时需要启动zooKeeper ,需要执行Provider或者启动tomcat服务，启动tomcat前需要已经启动zookeeper 如果配置了集群至少启动三个zookeeper  而且活动的zookeeper至少为2个(测试时当杀死多个zookeeper只剩下一个会报错)
  * leader死掉之后需要过一点时间才会执行成功(follower 转leader需要时间)
  */
 public class ConsumerZookeeper {
