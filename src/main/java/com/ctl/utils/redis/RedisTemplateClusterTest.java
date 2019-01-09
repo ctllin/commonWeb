eaa408ac05868d7c3780c11c19a9695e7b54a572
林家小院 <guolinit@163.com>
2019-01-09 16:13:03 +0800

    'modify'

--- src/main/java/com/ctl/utils/redis/RedisTemplateClusterTest.java
+++ src/main/java/com/ctl/utils/redis/RedisTemplateClusterTest.java
@@ -15,7 +15,7 @@ import org.springframework.data.redis.core.ValueOperations;
 * @date Apr 18, 2018  
 * @version 1.0  
 */
-public class Test {
+public class RedisTemplateClusterTest {
 	public static RedisTemplate redis;
 	public static ValueOperations valueOperations;
 
